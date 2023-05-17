dd4edf4605751d53c02040b847d9bce7f4585ea3
Anton Andryakov <60717352+Drentmort@users.noreply.github.com>
2023-05-17 11:50:30 +0300

    Add files via upload

---
+++ PollingClass.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerConsoleTest
+{
+    #region Poller
+    public enum PollType
+    {
+        Timer = 1,
+        Task = 2,
+        Thread = 3,
+        ThreadRecreate = 4
+    }
+
+    internal interface IPoller
+    {
+        void Start(int pollPeriod);
+        event Action OnPoll;
+    }
+
+    internal class TimerPoll : IPoller
+    {
+        private Timer _timer;
+
+        public TimerPoll()
+        {
+            _timer = new Timer(OnTimerTick, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public event Action OnPoll = delegate { };
+
+        public void Start(int pollPeriod)
+        {
+            _timer.Change(0, pollPeriod);
+        }
+
+        private void OnTimerTick(object o)
+        {
+            OnPoll();
+        }
+    }
+
+    internal class TaskPoll : IPoller
+    {
+        public event Action OnPoll = delegate{ };
+
+        public void Start(int pollPeriod)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    OnPoll();
+                    Thread.Sleep(pollPeriod);
+                }
+            });
+        }
+    }
+
+    internal class ThreadPoll : IPoller
+    {
+        public event Action OnPoll;
+
+        public void Start(int pollPeriod)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    OnPoll();
+                    Thread.Sleep(pollPeriod);
+                }
+            }).Start();
+        }
+    }
+
+    internal class ThreadRecreatePoll : IPoller
+    {
+        private int _pollPeriod;
+        public event Action OnPoll;
+
+        public void Start(int pollPeriod)
+        {
+            ThreadWorker worker = new ThreadWorker();
+            worker.ThreadDone += HandleThreadDone;
+
+            Thread thread1 = new Thread(() => worker.Run(_pollPeriod, OnPoll));
+            thread1.Start();
+        }
+
+        private void HandleThreadDone(object sender, EventArgs e)
+        {
+            ThreadWorker worker = new ThreadWorker();
+            worker.ThreadDone += HandleThreadDone;
+
+            Thread thread2 = new Thread(() => worker.Run(_pollPeriod, OnPoll));
+            thread2.Start();
+        }
+
+        class ThreadWorker
+        {
+            public event EventHandler ThreadDone;
+            public void Run(int pollPeriod, Action onPoll)
+            {
+                onPoll();
+                Thread.Sleep(pollPeriod);
+
+                if (ThreadDone != null)
+                    ThreadDone(this, EventArgs.Empty);
+            }
+        }
+    }
+    #endregion
+
+    #region Sync
+    public enum SyncObjLocalization
+    {
+        Local = 1,
+        Static = 2
+    }
+    public enum SyncObjType
+    {
+        Monitor = 1,
+        Mutex = 2,
+        Semaphore = 3,
+        ResetEvent = 4
+    }
+    internal interface ISycnObj
+    {
+        void Enter();
+        void Exit();
+    }
+
+    internal class MonitorSync : ISycnObj
+    {
+        private object _lock = new object(); 
+        public void Enter()
+        {
+            Monitor.Enter(_lock);
+        }
+
+        public void Exit()
+        {
+            Monitor.Exit(_lock);
+        }
+    }
+
+    internal class MutexSync : ISycnObj
+    {
+        private readonly Mutex _mutex = new Mutex();
+
+        public void Enter()
+        {
+            _mutex.WaitOne();
+        }
+
+        public void Exit()
+        {
+            _mutex.ReleaseMutex();
+        }
+    }
+
+    internal class SemaphoreSync:ISycnObj
+    {
+        private readonly Semaphore _semaphore = new Semaphore(1,1);
+
+        public void Enter()
+        {
+            _semaphore.WaitOne();
+        }
+
+        public void Exit()
+        {
+            _semaphore.Release();
+        }
+    }
+
+    internal class ResetEventSync : ISycnObj
+    {
+        private readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(true);
+        public void Enter()
+        {
+            _autoResetEvent.WaitOne();
+        }
+
+        public void Exit()
+        {
+            _autoResetEvent.Set();
+        }
+    }
+    #endregion
+
+    internal class PollingClass
+    {
+        public event Action<TimeSpan> PollLockHandler;
+        private IPoller _poller;
+        private ISycnObj _sycnObjLocal;
+        private static ISycnObj _sycnObjStatic;
+        private bool useLocal = true;
+        private int _immitationTime = 0;
+
+        public void Start(PollType timerType, SyncObjType syncType, SyncObjLocalization syncObjType, int pollPeriod, int immitationTime)
+        {
+            _immitationTime = immitationTime;
+            _poller = CreatePoller(timerType);
+            useLocal = syncObjType == SyncObjLocalization.Local;
+            if (useLocal)
+            {
+                _sycnObjLocal = CreateSyncObj(syncType);
+            }
+            else
+            {
+                _sycnObjStatic = CreateSyncObj(syncType);
+            }
+            _poller.OnPoll += DoSyncWithTimeMeasure;
+            _poller.Start(pollPeriod);
+        }
+
+        private static IPoller CreatePoller(PollType timerType)
+        {
+            switch(timerType) 
+            {
+                case PollType.Timer: 
+                    return new TimerPoll();
+                case PollType.Thread:
+                    return new ThreadPoll();
+                case PollType.Task:
+                    return new TaskPoll();
+                case PollType.ThreadRecreate:
+                    return new ThreadRecreatePoll();
+                default: 
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static ISycnObj CreateSyncObj(SyncObjType syncType)
+        {
+            switch (syncType) 
+            {
+                case SyncObjType.Monitor:
+                    return new MonitorSync();
+                case SyncObjType.Mutex:
+                    return new MutexSync();
+                case SyncObjType.ResetEvent:
+                    return new ResetEventSync();
+                case SyncObjType.Semaphore:
+                    return new SemaphoreSync();
+                default:
+                    throw new NotImplementedException();
+
+            }
+        }
+
+        private void DoSyncWithTimeMeasure()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            var sycnObj = useLocal 
+                ? _sycnObjLocal
+                : _sycnObjStatic;
+            sycnObj.Enter();
+            Thread.Sleep(_immitationTime);
+            sycnObj.Exit();
+            timer.Stop();
+
+            PollLockHandler?.Invoke(timer.Elapsed);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerConsoleTest
+{
+    internal class Program
+    {
+        private static ManualResetEvent @event = new ManualResetEvent(false);
+
+        private static double summ = 0;
+        private static double count = 0;
+
+        static void Main(string[] args)
+        {
+            WriteEmumChoiseToConsole<PollType>("Enter poll type");
+            var pollType = ReadEmumChoiseFromConsole<PollType>();
+
+            WriteEmumChoiseToConsole<SyncObjType>("Enter sych object type");
+            var syncObjType = ReadEmumChoiseFromConsole<SyncObjType>();
+
+            WriteEmumChoiseToConsole<SyncObjLocalization>("Enter sych object location");
+            var syncObjLocalization = ReadEmumChoiseFromConsole<SyncObjLocalization>();
+
+            var threadCount = WriteAndReadUInt32GetToConsole("Enter thread count", 1000);
+            
+            var waitMillisec = WriteAndReadUInt32GetToConsole("Enter poll step in milliseconds", 500);
+
+            var workImmitationTime = WriteAndReadUInt32GetToConsole("Enter imitation work time in milliseconds", 10);
+
+            Console.WriteLine("Every ready, enter any symbol to start..");
+            Console.ReadLine();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                var Poller = new PollingClass();
+                Poller.PollLockHandler += time =>
+                {
+                    Console.WriteLine($"{DateTime.Now} call takes {time.TotalMilliseconds} milliseconds");
+                    summ += time.TotalMilliseconds;
+                    count++;
+                    Console.WriteLine($"{DateTime.Now} average {summ / count} milliseconds");
+
+                };
+                Poller.Start(pollType, syncObjType, syncObjLocalization, (int)waitMillisec, (int)workImmitationTime);
+            }          
+            @event.WaitOne();
+        }
+
+        private static uint WriteAndReadUInt32GetToConsole(string calling, uint defaultVal)
+        {
+            Console.WriteLine($"{calling} (default {defaultVal})");
+            if (!uint.TryParse(Console.ReadLine(), out var output))
+            {
+                output = defaultVal;
+            }
+            return output;
+        }
+        
+        private static void WriteEmumChoiseToConsole<TEnum>(string calling)
+            where TEnum : struct
+        {
+            Console.WriteLine(calling);
+            foreach (var v in Enum.GetValues(typeof(TEnum)))
+            {
+                Console.WriteLine($"{v} - {(int)v}");
+            }
+        }
+
+        private static TEnum ReadEmumChoiseFromConsole<TEnum>()
+            where TEnum : struct
+        {
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out var outputNum) && Enum.IsDefined(typeof(TEnum), outputNum))
+            { 
+                return (TEnum)Enum.ToObject(typeof(TEnum), outputNum);
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+
+    
+}
